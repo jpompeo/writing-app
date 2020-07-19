40bab875e2d45dadb209cda6d24998f4c763efd9
jpompeo <59343333+jpompeo@users.noreply.github.com>
2020-07-19 00:00:56 -0400

    Mix chart data calculated, need to refactor chart type

--- server/client/src/components/Progress.js
+++ server/client/src/components/Progress.js
@@ -35,240 +35,237 @@ class Progress extends Component {
         if (this.props.currentBook.title) {
 
             if (direction === 'left') {
-                if (currentPage > 1) {
-                    currentPage--;
-                    this.setState({ currentPage: currentPage })
-                }
-            } else {
-                if (currentPage < this.state.totalPages) {
-                    currentPage++;
-                    this.setState({ currentPage: currentPage })
-                }
+                currentPage > 1 ? currentPage-- : currentPage = this.state.totalPages;
+                this.setState({ currentPage: currentPage })
+            } else if (direction === 'right') {
+               currentPage < this.state.totalPages ? currentPage++ : currentPage = 1;
+                this.setState({ currentPage: currentPage })
             }
         }
     }
 
-    // for pagination 
-    renderPage() {
-        let currentPage = this.state.currentPage;
-        if (this.props.currentBook.title) {
-            console.log("RENDER PAGE ", this.props.currentBook)
-            if (currentPage === 1) {
-                console.log("PAGE 1")
-                return this.renderTotalProgress();
-            } else if (currentPage === 2) {
-                return this.renderBurnDownChart();
-            } else if (currentPage === 3) {
-                return this.renderProjections();
-            }
-        } else {
-            return (
-                <h1 className="chart-header">Select a book</h1>
-            )
+
+// for pagination 
+renderPage() {
+    let currentPage = this.state.currentPage;
+    if (this.props.currentBook.title) {
+        console.log("RENDER PAGE ", this.props.currentBook)
+        if (currentPage === 1) {
+            console.log("PAGE 1")
+            return this.renderTotalProgress();
+        } else if (currentPage === 2) {
+            return this.renderBurnDownChart();
+        } else if (currentPage === 3) {
+            return this.renderProjections();
         }
+    } else {
+        return (
+            <h1 className="chart-header">Select a book</h1>
+        )
     }
+}
 
-    //Projections based on past data
-    renderProjections() {
-        let currentBook = this.props.currentBook;
-        let updates = this.props.updates;
+//Projections based on past data
+renderProjections() {
+    let currentBook = this.props.currentBook;
+    let updates = this.props.updates;
 
-        if (currentBook.title) {
-            return (
-                <MixChart/>
-            )
-        }
+    if (currentBook.title) {
+        return (
+            <MixChart />
+        )
     }
+}
 
-    // progress over time
-    renderBurnDownChart() {
-        const currentBook = this.props.currentBook
-        const updates = this.props.user.updates;
+// progress over time
+renderBurnDownChart() {
+    const currentBook = this.props.currentBook
+    const updates = this.props.user.updates;
 
-        if (currentBook.title) {
-            if (updates) {
-                //get updates for current book
-                let bookUpdates = updates.filter(update => {
-                    return update.bookTitle === currentBook.title;
-                })
+    if (currentBook.title) {
+        if (updates) {
+            //get updates for current book
+            let bookUpdates = updates.filter(update => {
+                return update.bookTitle === currentBook.title;
+            })
 
-                //sort by date
-                let sortedUpdates = _.sortBy(bookUpdates, (update => {
-                    let sortByDate = update.date || update.createdAt;
-                    return sortByDate;
-                }))
+            //sort by date
+            let sortedUpdates = _.sortBy(bookUpdates, (update => {
+                let sortByDate = update.date || update.createdAt;
+                return sortByDate;
+            }))
 
-                //calculate bar
-                const barDisplay = [];
-                sortedUpdates.reduce((length, update) => {
-                    console.log()
-                    let newLength = (length - update.progress);
-                    barDisplay.push(newLength);
-                    return newLength;
-                }, currentBook.expectedLength);
+            //calculate amount for each bar (inverted word count)
+            const barDisplay = [];
+            sortedUpdates.reduce((length, update) => {
+                console.log()
+                let newLength = (length - update.progress);
+                barDisplay.push(newLength);
+                return newLength;
+            }, currentBook.expectedLength);
 
-                //date labels for bars
-                const barDates = sortedUpdates.map(update => {
-                    return Moment(update.date || update.created).format('MMM DD')
-                })
+            //date labels for bars
+            const barDates = sortedUpdates.map(update => {
+                return Moment(update.date || update.created).format('MMM DD')
+            })
 
-                //first bar
-                const startDate = Moment(currentBook.startDate).format('MMM DD')
+            //first bar
+            const startDate = Moment(currentBook.startDate).format('MMM DD')
 
-                //to alternate colors
-                const graphColors = barDisplay.map((bar, index) => {
-                    let even = index % 2;
-                    return even ? "#979C9C" : "#DD8172";
-                })
+            //to alternate colors
+            const graphColors = barDisplay.map((bar, index) => {
+                let even = index % 2;
+                return even ? "#979C9C" : "#DD8172";
+            })
 
-                //full data to pass to graph
-                const bookData = {
-                    labels: [startDate, ...barDates],
-                    datasets: [
-                        {
-                            label: 'Word Count',
-                            backgroundColor: ['#979C9C', ...graphColors],
-                            borderColor: 'rgba(0,0,0,1)',
-                            borderWidth: 2,
-                            data: [currentBook.expectedLength, ...barDisplay, null]
-                        }
-                    ]
-                }
-
-                //render graph
-                return (
-                    <div>
-                        <h3 className="chart-header">Progress Over Time</h3>
-                        <Bar
-                            data={bookData}
-                            options={{
-                                title: {
-                                    display: false,
-                                },
-                                legend: {
-                                    display: false
-                                },
-                                aspectRatio: 1
-                            }}
-                        />
-                    </div>
-                )
-            }
-        }
-    }
-
-    //doughnut chart- overall percentage completed
-    renderTotalProgress() {
-        let currentBook = this.props.currentBook;
-        if (currentBook.title) {
-
-            //calculate chapter progress
-            // let chaptersTotal = currentBook.chapters.length;
-            // let chaptersCompleted = currentBook.chapters.filter(chapter => {
-            //     return chapter.completed === true
-            // }).length
-            // let percentChaptersCompleted = Math.round((chaptersCompleted / chaptersTotal) * 100);
-            // let percentChaptersLeftToComplete = 100 - percentChaptersCompleted;
-
-            //calculated overall progress
-            let bookTotalProgress = currentBook.progress;
-            let bookExpectedLength = currentBook.expectedLength;
-            let totalPercentCompleted = Math.round((bookTotalProgress / bookExpectedLength) * 100);
-
-            //set chart options
+            //full data to pass to graph
             const bookData = {
-                labels: ['Words Written', 'Remaining Words'],
+                labels: [startDate, ...barDates],
                 datasets: [
                     {
-                        label: 'Overall Progress',
-                        fill: true,
-                        lineTension: 0.5,
-                        backgroundColor: ['#F5D76B', '#979C9C'],
-                        borderColor: ['#979C9C', '#979C9C'],
+                        label: 'Word Count',
+                        backgroundColor: ['#979C9C', ...graphColors],
+                        borderColor: 'rgba(0,0,0,1)',
                         borderWidth: 2,
-                        data: [bookTotalProgress, bookExpectedLength]
+                        data: [currentBook.expectedLength, ...barDisplay, null]
                     }
-                ],
-                legend: {
-                    display: false
-                }
+                ]
             }
 
-            // render doughnut chart 
+            //render graph
             return (
-                <React.Fragment>
-                    <h3 className="chart-header">Current Progress &bull; {totalPercentCompleted}% Complete</h3>
-                    <Doughnut
+                <div>
+                    <h3 className="chart-header">Progress Over Time</h3>
+                    <Bar
                         data={bookData}
                         options={{
+                            title: {
+                                display: false,
+                            },
                             legend: {
                                 display: false
                             },
                             aspectRatio: 1
                         }}
                     />
-                    <h3 className="chart-header">"{currentBook.title}"</h3>
-                </React.Fragment>
+                </div>
             )
         }
     }
+}
 
-    // TODO: Make chart or delete
-    renderChapterProgress() {
-        if (this.props.currentBook.chapters) {
-            return this.props.currentBook.chapters.map(chapter => {
-                let chapterGoal = chapter.expectedLength;
-                let amountCompleted = chapter.progress;
-                let percentCompleted = Math.round((amountCompleted / chapterGoal) * 100);
-                let percentLeftToComplete = 100 - percentCompleted;
+//doughnut chart- overall percentage completed
+renderTotalProgress() {
+    let currentBook = this.props.currentBook;
+    if (currentBook.title) {
 
-                if (chapter.completed === true) {
-                    return (
-                        <li>Chapter {chapter.number} is complete!</li>
-                    )
-                } else {
+        //calculate chapter progress
+        // let chaptersTotal = currentBook.chapters.length;
+        // let chaptersCompleted = currentBook.chapters.filter(chapter => {
+        //     return chapter.completed === true
+        // }).length
+        // let percentChaptersCompleted = Math.round((chaptersCompleted / chaptersTotal) * 100);
+        // let percentChaptersLeftToComplete = 100 - percentChaptersCompleted;
 
-                    return (
-                        <li>Chapter {chapter.number} is {percentCompleted}% complete</li>
-                    )
+        //calculated overall progress
+        let bookTotalProgress = currentBook.progress;
+        let bookExpectedLength = currentBook.expectedLength;
+        let totalPercentCompleted = Math.round((bookTotalProgress / bookExpectedLength) * 100);
+
+        //set chart options
+        const bookData = {
+            labels: ['Words Written', 'Remaining Words'],
+            datasets: [
+                {
+                    label: 'Overall Progress',
+                    fill: true,
+                    lineTension: 0.5,
+                    backgroundColor: ['#F5D76B', '#979C9C'],
+                    borderColor: ['#979C9C', '#979C9C'],
+                    borderWidth: 2,
+                    data: [bookTotalProgress, bookExpectedLength]
                 }
-            })
+            ],
+            legend: {
+                display: false
+            }
         }
-    }
 
-
-    //final render of component
-    render() {
+        // render doughnut chart 
         return (
-            <Container id="progress-container" >
-                <Row>
-                    <Col id="progress-burndown-col">
-
-                        {/* left arrow button for pagination */}
-                        <div
-                            onClick={event => { this.handleArrowClick('left') }}
-                            id="left-arrow"
-                        >
-                            &lsaquo;
-                        </div>
-
-                        {/* Chart view based on page number  */}
-                        <div id="progress-charts">
-                            {this.renderPage()}
-                        </div>
-
-                        {/* right arrow for pagination  */}
-                        <div
-                            id="right-arrow"
-                            onClick={event => { this.handleArrowClick('right') }}
-                        >
-                            &rsaquo;
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+            <React.Fragment>
+                <h3 className="chart-header">Current Progress &bull; {totalPercentCompleted}% Complete</h3>
+                <Doughnut
+                    data={bookData}
+                    options={{
+                        legend: {
+                            display: false
+                        },
+                        aspectRatio: 1
+                    }}
+                />
+                <h3 className="chart-header">"{currentBook.title}"</h3>
+            </React.Fragment>
         )
     }
+}
+
+// TODO: Make chart or delete
+renderChapterProgress() {
+    if (this.props.currentBook.chapters) {
+        return this.props.currentBook.chapters.map(chapter => {
+            let chapterGoal = chapter.expectedLength;
+            let amountCompleted = chapter.progress;
+            let percentCompleted = Math.round((amountCompleted / chapterGoal) * 100);
+            let percentLeftToComplete = 100 - percentCompleted;
+
+            if (chapter.completed === true) {
+                return (
+                    <li>Chapter {chapter.number} is complete!</li>
+                )
+            } else {
+
+                return (
+                    <li>Chapter {chapter.number} is {percentCompleted}% complete</li>
+                )
+            }
+        })
+    }
+}
+
+
+//final render of component
+render() {
+    return (
+        <Container id="progress-container" >
+            <Row>
+                <Col id="progress-burndown-col">
+
+                    {/* left arrow button for pagination */}
+                    <div
+                        onClick={event => { this.handleArrowClick('left') }}
+                        id="left-arrow"
+                    >
+                        &lsaquo;
+                        </div>
+
+                    {/* Chart view based on page number  */}
+                    <div id="progress-charts">
+                        {this.renderPage()}
+                    </div>
+
+                    {/* right arrow for pagination  */}
+                    <div
+                        id="right-arrow"
+                        onClick={event => { this.handleArrowClick('right') }}
+                    >
+                        &rsaquo;
+                        </div>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
 }
 
 
--- server/client/src/components/charts/LineGraph.js
+++ server/client/src/components/charts/LineGraph.js
@@ -1,50 +1,224 @@
-import React from 'react';
-import {Line} from 'react-chartjs-2';
+import React, { Component } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import Moment from 'moment';
+import _ from 'lodash';
 
-const state = {
-  labels: ['Monday', 'Tuesday', 'Wednesday',
-           'Thursday', 'Friday', 'Saturday', 'Sunday'],
-  datasets: [
-    {
-      label: 'Goal',
-      fill: false,
-      lineTension: 0,
-      backgroundColor: 'rgba(0,0,0,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [1000, 1000, 1000, 1000, 1000, 1000, 1000]
-    },
-    {
-      label: 'Word Count',
-      fill: true,
-      lineTension: 0.5,
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [1500, 500, 3000, 1000, 2000, 400, ]
+
+
+class MixChart extends Component {
+    constructor(props) {
+        super(props)
+
+        this.renderMixChart = this.renderMixChart.bind(this);
     }
-  ]
+
+    renderMixChart() {
+        let currentBook = this.props.currentBook;
+        let updates = this.props.updates;
+
+        if (currentBook.title && updates) {
+
+            //for line description
+            const plugins = [{
+                afterDraw: (chartInstance, easing) => {
+                    const ctx = chartInstance.chart.ctx;
+                    ctx.fillText("This text drawn by a plugin", 100, 100);
+                }
+            }];
+
+            //chart configuration
+            const options = {
+                responsive: true,
+                tooltips: {
+                    mode: 'label'
+                },
+                elements: {
+                    line: {
+                        fill: false
+                    }
+                },
+                scales: {
+                    xAxes: [
+                        {
+                            display: true,
+                            gridLines: {
+                                display: false
+                            },
+                            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                        }
+                    ],
+                    yAxes: [
+                        {
+                            type: 'linear',
+                            display: true,
+                            position: 'left',
+                            id: 'y-axis-1',
+                            gridLines: {
+                                display: false
+                            },
+                            labels: {
+                                show: true
+                            }
+                        },
+                        {
+                            type: 'linear',
+                            display: true,
+                            position: 'right',
+                            id: 'y-axis-2',
+                            gridLines: {
+                                display: false
+                            },
+                            labels: {
+                                show: true
+                            }
+                        }
+                    ]
+                }
+            };
+
+            // calculate data needed for chart //
+
+            //get updates for current book
+            let bookUpdates = updates.filter(update => {
+                return update.bookTitle === currentBook.title;
+            })
+
+            //sort by date
+            let sortedUpdates = _.sortBy(bookUpdates, (update => {
+                let sortByDate = update.date || update.createdAt;
+                return sortByDate;
+            }))
+
+            //get total word count
+            let totalWordCount = bookUpdates.reduce((total, update) => {
+                return total + update.progress;
+            }, 0);
+
+            // divide word count into days (goal start till today) //
+            //get dates needed
+            const goalStart = Moment(currentBook.startDate);
+            const today = Moment(new Date());
+            const goalEnd = Moment(currentBook.deadline);
+
+            //Differences in number of days
+            let totalDays = Math.abs(Math.round(Moment.duration(goalStart.diff(today)).asDays()));
+            let daysRemaining = Math.abs(Math.round(Moment.duration(today.diff(goalEnd)).asDays()));
+
+            //Differences in number of weeks
+            let totalWeeks = Math.abs(Math.round(Moment.duration(goalStart.diff(today)).asWeeks()));
+            let weeksRemaining = Math.abs(Math.round(Moment.duration(today.diff(goalEnd)).asWeeks()));
+
+            //Differences in number of weeks
+            let totalMonths = Math.abs(Math.round(Moment.duration(goalStart.diff(today)).asMonths()));
+            let monthsRemaining = Math.abs(Math.round(Moment.duration(today.diff(goalEnd)).asMonths()));
+
+            //get averages daily, weekly, monthly word count
+            let dailyAverage = Math.round(totalWordCount / totalDays);
+            let weeklyAverage = Math.round(totalWordCount / totalWeeks);
+            let monthlyAverage = Math.round(totalWordCount / totalMonths);
+
+            console.log("Averages", dailyAverage, weeklyAverage, monthlyAverage)
+            console.log("totals", totalDays, totalWeeks, totalMonths)
+
+            //get daily word goal
+            let dailyGoal = Math.round(currentBook.expectedLength / (totalDays + daysRemaining))
+            
+            //map average and goal to array for graph
+            let averageLineData = bookUpdates.map(update => {
+                return dailyAverage;
+            })
+
+            let goalLineData = bookUpdates.map(update => {
+                return dailyGoal;
+            })
+
+            //get daily actual word counts
+            let actualData = sortedUpdates.map(update => {
+                return update.progress;
+            });
+
+            
+            //chart data
+            const data = {
+
+                datasets: [{
+                    label: 'Daily Average',
+                    type: 'line',
+                    data: [...averageLineData],
+                    fill: true,
+                    borderColor: '#EC932F',
+                    backgroundColor: '#EC932F',
+                    pointBorderColor: '#EC932F',
+                    pointBackgroundColor: '#EC932F',
+                    pointHoverBackgroundColor: '#EC932F',
+                    pointHoverBorderColor: '#EC932F',
+                    yAxisID: 'y-axis-2'
+                },
+                {
+                    label: 'Daily Goal',
+                    type: 'line',
+                    data: [...goalLineData],
+                    fill: false,
+                    borderColor: '#979C9C',
+                    backgroundColor: '#979C9C',
+                    pointBorderColor: '#EC932F',
+                    pointBackgroundColor: '#EC932F',
+                    pointHoverBackgroundColor: '#EC932F',
+                    pointHoverBorderColor: '#EC932F',
+                    yAxisID: 'y-axis-2'
+                },
+                {
+                    type: 'bar',
+                    label: 'Actual Word Count',
+                    data: [...actualData],
+                    fill: false,
+                    backgroundColor: '#71B37C',
+                    borderColor: '#71B37C',
+                    hoverBackgroundColor: '#71B37C',
+                    hoverBorderColor: '#71B37C',
+                    yAxisID: 'y-axis-1'
+                }]
+            };
+
+            return (
+                <React.Fragment>
+
+                    <h2>Mixed data Example</h2>
+                    <Bar
+                        data={data}
+                        options={options}
+                        plugins={plugins}
+                    />
+
+                </React.Fragment>
+
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderMixChart()}
+            </div>
+        );
+    }
+};
+
+function mapStateToProps(state) {
+    return {
+        updates: state.userData.updates,
+        currentBook: state.currentBook
+    };
 }
 
-export default class LineGraph extends React.Component {
-  render() {
-    return (
-      <div>
-        <Line
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Weekly Goal',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-      </div>
-    );
-  }
-}
+// function mapDispatchToProps(dispatch) {
+//     return bindActionCreators(
+//         { logoutUser },
+//         dispatch
+//     );
+//   }
+
+export default connect(mapStateToProps, null)(MixChart);
